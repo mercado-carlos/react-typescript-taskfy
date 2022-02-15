1b2a9f773f36beb3f0dbaf9bb6d539c6fffb5fd0
mercado-carlos <carlos-roberto.mercado@atos.net>
2022-02-15 16:52:46 -0600

    SingleTodo Component

--- src/components/SingleTodo.tsx
+++ src/components/SingleTodo.tsx
@@ -1,6 +1,11 @@
 import React from 'react';
 import { Todo } from '../model';
 
+import { AiFillEdit, AiFillDelete } from 'react-icons/ai';
+import { MdDone } from 'react-icons/md';
+
+import './style.css';
+
 type Props = {
     todo: Todo;
     todos: Todo[];
@@ -8,7 +13,22 @@ type Props = {
 };
 
 const SingleTodo = ({ todo, todos, setTodos }: Props) => {
-    return <div>SingleTodo</div>;
+    return (
+        <form className="todos__single">
+            <span className="todos__single--text">{todo.todo}</span>
+            <div>
+                <span className="icon">
+                    <AiFillEdit />
+                </span>
+                <span className="icon">
+                    <AiFillDelete />
+                </span>
+                <span className="icon">
+                    <MdDone />
+                </span>
+            </div>
+        </form>
+    );
 };
 
 export default SingleTodo;
