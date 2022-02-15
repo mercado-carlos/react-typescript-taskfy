3ba8a386b6a37b0ae8e9c1ed30c12fd24f523fbe
mercado-carlos <carlos-roberto.mercado@atos.net>
2022-02-15 16:39:09 -0600

    Passing props to SingleTodo

--- src/components/SingleTodo.tsx
+++ src/components/SingleTodo.tsx
@@ -1,6 +1,13 @@
 import React from 'react';
+import { Todo } from '../model';
 
-const SingleTodo = () => {
+type Props = {
+    todo: Todo;
+    todos: Todo[];
+    setTodos: React.Dispatch<React.SetStateAction<Todo[]>>;
+};
+
+const SingleTodo = ({ todo, todos, setTodos }: Props) => {
     return <div>SingleTodo</div>;
 };
 
