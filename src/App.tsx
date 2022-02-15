a1ceebd5aa00edde478fa85eb727123082efba8c
mercado-carlos <carlos-roberto.mercado@atos.net>
2022-02-15 13:32:54 -0600

    Basic Types

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,12 @@
 import React from 'react';
 import './App.css';
 
+let name: string;
+let age: number;
+let isStudent: boolean;
+let hobbies: string[];
+let role: [number, string];
+
 function App() {
     return <div className="App">Hello world</div>;
 }
