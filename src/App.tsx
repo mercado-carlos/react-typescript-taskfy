d08d36545a78dae4e3c902b9278b7fddfee9b47d
mercado-carlos <carlos-roberto.mercado@atos.net>
2022-02-15 14:01:41 -0600

    unknown and never Type

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ let isStudent: boolean;
 let hobbies: string[];
 let role: [number, string];
 
-let printName: (name: string) => void;
+let printName: (name: string) => never;
 /* function printName(name: string) {
     console.log(name);
 }
@@ -22,6 +22,8 @@ let person: Person = {
     //age: 23,
 };
 let lotsOfPeople: Person[]; */
+
+let personName: unknown;
 
 function App() {
     return <div className="App">Hello world</div>;
