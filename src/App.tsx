eebb2c0d5d79d61cb4ea3e48ecaca74edf81bb45
mercado-carlos <carlos-roberto.mercado@atos.net>
2022-02-16 10:23:18 -0600

    completedTodos States and DnD npm

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import { Todo } from './model';
 const App: React.FC = () => {
     const [todo, setTodo] = useState<string>('');
     const [todos, setTodos] = useState<Todo[]>([]);
+    const [completedTodos, setCompletedTodos] = useState<Todo[]>([]);
 
     const handleAdd = (e: React.FormEvent) => {
         e.preventDefault();
@@ -23,7 +24,12 @@ const App: React.FC = () => {
         <div className="App">
             <span className="heading">Taskfy</span>
             <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd} />
-            <TodoList todos={todos} setTodos={setTodos} />
+            <TodoList
+                todos={todos}
+                setTodos={setTodos}
+                completedTodos={completedTodos}
+                setCompletedTodos={setCompletedTodos}
+            />
         </div>
     );
 };
